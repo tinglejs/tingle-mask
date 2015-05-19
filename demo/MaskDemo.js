81cb40914ac69e46affe426f4bf8a08de872ab8a
拂山 <jiasong.js@alibaba-inc.com>
2015-05-19 16:15:55 +0800

    init

---
+++ demo/MaskDemo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Mask} from '../src';
+
+// TODO: move the line to tingle-env
+React.initializeTouchEvents(true);
+
+class Demo extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    // 以私有方式使用`mask`，要显示声明`Mask Component`, 
+    // 参数通过`Component`的`props`实现
+    showPrivateMask() {
+        this.refs.m1.show();
+    }
+
+    // 以全局方式使用`mask`，不需要显示声明`Mask Component`, 
+    // 参数通过`Mask.global.show(options)`实现
+    showGlobalMask() {
+        Mask.global.show({
+            opacity: 0.2,
+            onHide: function () {}
+        });
+    }
+
+    render() {
+        return (
+            <div className="tP10">
+                <h3 className="tF18 tMB6" onClick={this.showPrivateMask.bind(this)}>点击打开私有mask</h3>
+                <Mask ref="m1" opacity={0.5} className="m1"/>
+                <p className="tMB20 tF6">以私有方式使用`mask`，要显示声明`Mask Component`, 参数通过`Component`的`props`实现</p>
+                <h3 className="tF18 tMB6" onClick={this.showGlobalMask.bind(this)}>点击打开全局mask</h3>
+                <p className="tMB20 tF6">以全局方式使用`mask`，不需要显示声明`Mask Component`, 参数通过`Mask.global.show(options)`实现</p>
+            </div>
+        );
+    }
+};
+
+React.render(<Demo/>, document.getElementById('TingleDemo'));
---
+++ src/Mask.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+class Mask extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            opacity: props.opacity,
+            zIndex: props.zIndex,
+            onHide: props.onHide
+        }
+    }
+
+    componentDidMount() {
+        this.el = this.refs.el.getDOMNode();
+    }
+
+    /*
+        options.onHide
+        options.onClick
+        options.opacity
+    */
+    show(options) {
+        options = options || {};
+        var t = this;
+        t.el.classList.add('visible');
+        t.el.offsetWidth;
+        t.setState({
+            opacity: options.opacity || t.props.opacity,
+            onClick: options.onClick || t.props.onClick,
+            onHide:  options.onHide  || t.props.onHide
+        });
+        t.el.style.opacity = 1;
+    }
+
+    hide() {
+        var t = this;
+        t.el.style.opacity = 0;
+        setTimeout(function () {
+            t.el.classList.remove('visible');
+            t.state.onHide.call(t);
+        }, 200);
+    }
+
+    render() {
+        var t = this;
+        var cls = React.addons.classSet;
+
+        cls = cls({
+            tMask: true,
+            [t.props.className]: !!t.props.className
+        });
+
+        return (
+            <div ref="el" className={cls} style={{
+                backgroundColor: 'rgba(0, 0, 0, '+ t.state.opacity +')',
+                zIndex:  t.state.zIndex
+            }} onClick={t.hide.bind(this)}>
+            </div>
+        );
+    }
+}
+
+Mask.defaultProps = {
+    className: '',
+    zIndex: 1000,
+    opacity: 0,
+    visible: false,
+    onClick: function () {},
+    onHide: function () {}
+}
+
+Mask.global = React.render(<Mask/>, document.getElementById('TingleMask'));
+
+export {Mask};
